7b2d7fbac76ac46e090369620d8496a52fa57372
aarontam <aarontam@users.noreply.github.com>
2014-07-03 01:22:41 -0700

    Update viewPool.js

--- source/views/viewPool.js
+++ source/views/viewPool.js
@@ -1,6 +1,14 @@
 /**
-* Represents a view of a given prediction pool. This consists of the original prediction event
-* itself, as well as all of the predictions that have been made.
+* Represents a view of a given hunch pool. This consists of the original hunch event
+* itself, as well as all of the hunches that have been made. For example:
+* ```
+* Who will win the 2014 FIFA World Cup?
+* 
+* John's hunch: _Brazil_
+* Jane's hunch: _Argentina_
+* Scott's hunch: _Argentina_
+* Sam's hunch: _Belgium_
+* Lucy's hunch: _Colombia_
 *
 * @class nos.PoolView
 * @public
