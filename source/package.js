a64fcb2aafc7e228b1f0d1eea1db2de5bd01f9b2
Aaron Tam <aarontam@gmail.com>
2014-07-04 10:03:43 -0700

    Update package to reference mochi instead of onyx.

--- source/package.js
+++ source/package.js
@@ -1,8 +1,8 @@
 enyo.depends(
 	// Layout library
 	"$lib/layout",
-	// Onyx UI library
-	"$lib/onyx",	// To theme Onyx using Theme.less, change this line to $lib/onyx/source,
+	// Mochi UI library
+	"$lib/mochi",
 	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
 	// CSS/LESS style files
 	"style",
