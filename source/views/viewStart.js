62e0c3522ebfaf852551fc74a27b5f545a227e97
Aaron Tam <aarontam@gmail.com>
2014-07-04 10:02:38 -0700

    Remove onyx from view.

--- source/views/viewStart.js
+++ source/views/viewStart.js
@@ -2,23 +2,13 @@
 * The starting view that provides an introduction to `nos` and displays public predictions as well
 * as calls to action to create prediction pools.
 *
+* @TODO convert views to panels
+*
 * @class nos.StartView
 * @public
 */
 enyo.kind({
 	name: "nos.StartView",
 	kind: "FittableRows",
-	fit: true,
-	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
-		{kind: "enyo.Scroller", fit: true, components: [
-			{name: "main", classes: "nice-padding", allowHtml: true}
-		]},
-		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
-		]}
-	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.addContent("The button was tapped.<br/>");
-	}
+	fit: true
 });
